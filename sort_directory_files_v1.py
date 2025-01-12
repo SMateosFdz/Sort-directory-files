1585a84d569479a110a8ade0a1cf6cfc335bd221
sanmate <santiago.mateos@estudiantes.uva.es>
2025-01-12 19:43:23 +0100

    Update of the code, allowing the user to choose between creating a new directory for a certain extension or not

--- sort_directory_files_v1.py
+++ sort_directory_files_v1.py
@@ -4,15 +4,34 @@ import os, shutil
 directory = os.getcwd() 
 files = os.listdir(directory)
 
+
 for file in files:
-    
-    if file != "sort_directory_files.py":
-        name, extension = os.path.splitext(file)
+    flag = False #flag used to allow the user to choose if create a directory to a certain extension or not
 
+    if file != os.path.basename(__file__): #avoid to move this python file, but not others
+        name, extension = os.path.splitext(file) #split the filename into name and extension
+        
         if extension:
-            new_directory = os.path.join(directory, extension[1:]) # create new directories with extension names
+            while flag == False:
 
-            if not os.path.exists(new_directory):
-                os.makedirs(new_directory)
+                print("Do you want to sort the " + extension + " extension? 1 - Yes, 2 - No")
+                value = input()
 
-            shutil.move(os.path.join(directory, file), os.path.join(new_directory, file)) # move the files
+                try:
+                    if int(value) == 1:
+                        new_directory = os.path.join(directory, extension[1:]) # create new directories with extension names
+
+                        if not os.path.exists(new_directory):
+                            os.makedirs(new_directory)
+
+                        shutil.move(os.path.join(directory, file), os.path.join(new_directory, file)) # move the files
+                        flag = True
+
+                    elif int(value) == 2:
+                        flag = True
+                    
+                    else:
+                        print("Input incorrect, try again")
+
+                except:
+                    print("Input incorrect, try again")
